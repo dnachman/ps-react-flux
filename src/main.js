b82d800fad7b9c3afba3ae1013d8dd982ba71d35
David Nachman <david@nachman.org>
2016-05-23 18:24:26 -0400

    first components created

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
-
 $ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
 
-var App = console.log("Hello world from browserify");
+var App = React.createClass({
+  render: function() {
+    var Child;
+    
+    switch(this.props.route) {
+      case 'about': Child = About; break;
+      default: Child = Home;
+    } 
+    
+    return (
+      <div>
+        <Child />
+      </div>
+    );
+  }
+});
 
-module.exports = App;
+function render() {
+  var route = window.location.hash.substr(1);
+  React.render(<App route={route} />, document.getElementById('app'));
+}
+
+window.addEventListener('hashchange', render);
+render();
