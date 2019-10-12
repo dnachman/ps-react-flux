11d96246ab6f78d2beb55607f0e76bec7c7f4f97
David Nachman <david@nachman.org>
2019-10-12 10:29:06 -0400

    multiple components

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+	render() {
+		return (
+			<>
+				<h2>About</h2>
+				<p>This app used React.</p>
+			</>
+		);
+	}
+
+}
+
+export default AboutPage;
