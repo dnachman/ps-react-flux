01db1259378b6a1d08c7c8e31dc0ac94dccd98a0
Nachman, David (CORP) <David.Nachman@adp.com>
2019-10-14 17:08:20 -0400

    404s

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,19 @@ import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import CoursesPage from './CoursesPage';
 import Header from './common/Header';
-import { Route } from "react-router-dom";
+import { Route, Switch } from 'react-router-dom';
+import NotFoundPage from './NotFoundPage';
 
 function App() {
   return (
     <div className='container-fluid'>
       <Header />
-      <Route path="/" exact component={HomePage} />
-      <Route path="/courses" component={CoursesPage} />
-      <Route path="/about" component={AboutPage} />
+      <Switch>
+        <Route path='/' exact component={HomePage} />
+        <Route path='/courses' component={CoursesPage} />
+        <Route path='/about' component={AboutPage} />
+				<Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }
