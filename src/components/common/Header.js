16a29fcf5875132092684e2ccabcf1bcb0bb096c
Nachman, David (CORP) <David.Nachman@adp.com>
2019-10-14 17:02:50 -0400

    navlinks and styles

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
-	return (
-		<nav>
-			<a href="/">Home</a> | <a href="/about">About</a> | <a href="/courses">Courses</a>
-		</nav>
-	);
+  const activeStyle = { color: 'orange' };
+  return (
+    <nav>
+      <NavLink activeStyle={activeStyle} exact to='/'>
+        Home
+      </NavLink>
+      {' | '}
+      <NavLink activeStyle={activeStyle} to='/about'>
+        About
+      </NavLink>
+      {' | '}
+      <NavLink activeStyle={activeStyle} to='/courses'>
+        Courses
+      </NavLink>
+    </nav>
+  );
 }
 
 export default Header;
