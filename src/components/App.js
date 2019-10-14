08ac660e5a32ed430bc94d77be847cdd01a52910
Nachman, David (CORP) <David.Nachman@adp.com>
2019-10-14 16:53:33 -0400

    begin routers

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,15 @@ import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import CoursesPage from './CoursesPage';
 import Header from './common/Header';
+import { Route } from "react-router-dom";
 
 function App() {
-  function getPage() {
-    const route = window.location.pathname;
-    if (route === '/about') return <AboutPage />;
-    if (route === '/courses') return <CoursesPage />;
-    return <HomePage />;
-  }
   return (
     <div className='container-fluid'>
       <Header />
-      {getPage()}
+      <Route path="/" exact component={HomePage} />
+      <Route path="/courses" component={CoursesPage} />
+      <Route path="/about" component={AboutPage} />
     </div>
   );
 }
