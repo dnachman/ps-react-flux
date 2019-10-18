0aece53c318edde67e5264aca1be9f44c48a4265
David Nachman <david@nachman.org>
2019-10-17 21:04:49 -0400

    remove prompt

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Prompt } from "react-router-dom";
 
 const ManageCoursePage = props => {
 	return (
 		<>
 			<h2>Manage Course</h2>
-			<Prompt when={true} message="Are you sure you want to leave?" />
 			{props.match.params.slug}
 		</>
 	);
