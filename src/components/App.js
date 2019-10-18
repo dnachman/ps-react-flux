bd846b4a9f0e256ac4d75a485a527d03918b1569
David Nachman <david@nachman.org>
2019-10-17 21:04:36 -0400

    all react router feature demos

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import CoursesPage from './CoursesPage';
 import Header from './common/Header';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import NotFoundPage from './NotFoundPage';
+import ManageCoursePage from './ManageCoursePage';
 
 function App() {
   return (
@@ -13,7 +14,9 @@ function App() {
       <Switch>
         <Route path='/' exact component={HomePage} />
         <Route path='/courses' component={CoursesPage} />
+        <Route path='/course/:slug' component={ManageCoursePage} />
         <Route path='/about' component={AboutPage} />
+				<Redirect from='/about-page' to='/about' />
 				<Route component={NotFoundPage} />
       </Switch>
     </div>
